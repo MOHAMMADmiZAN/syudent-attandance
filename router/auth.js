174ec60855fd52a69b15e13a6942b506d4773147
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2022-03-29 22:18:35 +0600

    error code fix

--- router/auth.js
+++ router/auth.js
@@ -1,7 +1,7 @@
 const {registerController,loginController} = require('../controllers/auth');
 const Auth = require("../middleware/Auth");
 const router = require('express').Router();
- // const {registerRules}=require('../utils/validation-rules')
+
 const {body, validationResult} = require("express-validator");
 
 
@@ -9,7 +9,6 @@ const {body, validationResult} = require("express-validator");
 const registerRules = [body('name').trim().isEmpty().withMessage('Name is required').bail().isString().withMessage('Name must be a string'), body('email').isEmpty().withMessage('email is required').bail().normalizeEmail({all_lowercase: true}).isEmail().withMessage('Email not valid'), body('password').isEmpty().withMessage('Password is required').bail().isLength({min: 6}).withMessage('Password must be at least 6 characters long').bail().matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/).withMessage('Password must Be Valid'),];
 const rulesMessage = (req, res) => {
     const errors = validationResult(req);
-
     if (!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
     }
