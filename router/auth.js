34294493ac4e11d8a3a1e5c65b542820ddf749ff
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2022-03-29 22:57:12 +0600

    error code fix

--- router/auth.js
+++ router/auth.js
@@ -1,19 +1,11 @@
 const {registerController,loginController} = require('../controllers/auth');
 const Auth = require("../middleware/Auth");
 const router = require('express').Router();
-
-const {body, validationResult} = require("express-validator");
+const {registerRules, rulesMessage} = require("../utils/validationRules");
 
 
 // GET /auth/login
-const registerRules = [body('name').trim().isEmpty().withMessage('Name is required').bail().isString().withMessage('Name must be a string'), body('email').isEmpty().withMessage('email is required').bail().normalizeEmail({all_lowercase: true}).isEmail().withMessage('Email not valid'), body('password').isEmpty().withMessage('Password is required').bail().isLength({min: 6}).withMessage('Password must be at least 6 characters long').bail().matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/).withMessage('Password must Be Valid'),];
-const rulesMessage = (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({errors: errors.array()});
-    }
 
-}
 
 router.post('/register',...registerRules,rulesMessage,registerController);
 router.post('/login', loginController)
