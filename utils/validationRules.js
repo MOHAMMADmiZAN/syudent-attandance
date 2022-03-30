a18bd5b971a66ea9c69aa51a68201dea3ee3e3b9
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2022-03-30 16:02:38 +0600

    express validation fixed

--- utils/validationRules.js
+++ utils/validationRules.js
@@ -10,12 +10,12 @@ const registerRules = [
         .bail()
         .matches(/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,10})$/).withMessage("Email is invalid")
         .custom(async (value) => {
-        const user = await findUser('email', value);
-        if (user) {
-            throw new Error("Email already exists");
-        }
-        return true;
-    }),
+            const user = await findUser('email', value);
+            if (user) {
+                throw new Error("Email already exists");
+            }
+            return true;
+        }),
     body("password").not().trim().isEmpty().withMessage("Password is required")
         .bail()
         .isLength({min: 8}).withMessage("Password must be at least 8 characters long")
@@ -32,13 +32,10 @@ const registerRules = [
  */
 const rulesMessage = (req, res, next) => {
     const errors = validationResult(req);
-    console.log(req.body)
     if (!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
     }
     next();
-
-
 }
 
 module.exports = {
