4df2993a1dabe448e22ffc2926513f9bd8469490
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2022-03-31 00:50:15 +0600

    emailValid

--- utils/joiValidationRules.js
+++ utils/joiValidationRules.js
@@ -6,29 +6,42 @@ const Joi = require('joi');
  * @type {Joi.ObjectSchema<any>}
  */
 const registerSchema = Joi.object().keys({
-    name: Joi.string().trim().alphanum().min(3).max(30).required(),
-    email: Joi.string().trim().email().regex(/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,10})$/).required(),
-    password: Joi.string().trim().min(8).max(30).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$/).required()
+    name: Joi.string()
+        .trim()
+        .alphanum()
+        .min(3)
+        .max(30).required(),
+    email: Joi.string()
+        .trim()
+        .email({minDomainSegments: 2, tlds: {allow: ['com', 'net','org']}})
+        .regex(/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,10})$/)
+        .required(),
+    password: Joi.string()
+        .trim()
+        .min(8)
+        .max(30)
+        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$/)
+        .required()
 });
 /**
  *
  * @param {Joi.ObjectSchema} schema
  * @returns {(function(*, *, *))|*}
  */
- const schemaError = (schema)=>{
-     return (req,res,next)=> {
-         schema.validateAsync(req.body)
-             .then(()=>{
-                 next();
-             })
-             .catch((err)=>{
-                 res.status(400).json({
-                     message: err.details[0].context.key+err.details[0].message.split('\"')[2],
+const schemaError = (schema) => {
+    return (req, res, next) => {
+        schema.validateAsync(req.body)
+            .then(() => {
+                next();
+            })
+            .catch((err) => {
+                res.status(400).json({
+                    message: err.details[0].context.key + err.details[0].message.split('\"')[2],
 
-                 });
-             });
-     }
- }
+                });
+            });
+    }
+}
 
 module.exports = {
     registerSchema,
