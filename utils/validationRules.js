53d28602b3184d202760b4d8cc699e5ea20bae38
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2022-03-30 15:37:39 +0600

    error code fix

--- utils/validationRules.js
+++ utils/validationRules.js
@@ -1,12 +1,34 @@
 const {body, validationResult} = require("express-validator");
 
-const registerRules = [body('name').trim().isEmpty().withMessage('Name is required').bail().isString().withMessage('Name must be a string'), body('email').isEmpty().withMessage('email is required').bail().normalizeEmail({all_lowercase: true}).isEmail().withMessage('Email not valid'), body('password').isEmpty().withMessage('Password is required').bail().isLength({min: 6}).withMessage('Password must be at least 6 characters long').bail().matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/).withMessage('Password must Be Valid')];
-const rulesMessage = (req, res) => {
+const {findUser} = require("../services/user");
+
+const registerRules = [
+    body("name").not().trim().isEmpty().withMessage("Name is required"),
+    body("email").not().trim().isEmpty().withMessage("Email is required").isEmail().withMessage("Email is invalid").matches(/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,10})$/).withMessage("Email is invalid").custom(async (value) => {
+        const user = await findUser('email', value);
+        if (user) {
+            throw new Error("Email already exists");
+        }
+        return true;
+    }),
+    body("password").not().trim().isEmpty().withMessage("Password is required").isLength({min: 8}).withMessage("Password must be at least 8 characters long").matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/).withMessage("Password must contain at least one uppercase letter, one lowercase letter, one number and one special character"),
+
+]
+/**
+ *
+ * @param req
+ * @param res
+ * @param next
+ * @returns {*}
+ */
+const rulesMessage = (req, res, next) => {
     const errors = validationResult(req);
     console.log(req.body)
     if (!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
     }
+    next();
+
 
 }
 
