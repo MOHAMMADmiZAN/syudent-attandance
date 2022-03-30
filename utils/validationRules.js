219a0b04aaf6818294dc1a70deb7f2b6ce950a95
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2022-03-30 15:41:34 +0600

    express validation fixed

--- utils/validationRules.js
+++ utils/validationRules.js
@@ -4,14 +4,23 @@ const {findUser} = require("../services/user");
 
 const registerRules = [
     body("name").not().trim().isEmpty().withMessage("Name is required"),
-    body("email").not().trim().isEmpty().withMessage("Email is required").isEmail().withMessage("Email is invalid").matches(/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,10})$/).withMessage("Email is invalid").custom(async (value) => {
+    body("email").not().trim().isEmpty().withMessage("Email is required")
+        .bail()
+        .isEmail().withMessage("Email is invalid")
+        .bail()
+        .matches(/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,10})$/).withMessage("Email is invalid")
+        .custom(async (value) => {
         const user = await findUser('email', value);
         if (user) {
             throw new Error("Email already exists");
         }
         return true;
     }),
-    body("password").not().trim().isEmpty().withMessage("Password is required").isLength({min: 8}).withMessage("Password must be at least 8 characters long").matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/).withMessage("Password must contain at least one uppercase letter, one lowercase letter, one number and one special character"),
+    body("password").not().trim().isEmpty().withMessage("Password is required")
+        .bail()
+        .isLength({min: 8}).withMessage("Password must be at least 8 characters long")
+        .bail()
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/).withMessage("Password must contain at least one uppercase letter, one lowercase letter, one number and one special character"),
 
 ]
 /**
